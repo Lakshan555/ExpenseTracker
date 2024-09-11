dc3aed8c63040bfc8c28a723df840a1eb746508d
Ishanka <ishankal@orelit.com>
2024-09-11 12:01:03 +0530

    "Added DashboardHeader component to DashboardLayout and removed bg-blue-500 class from div element."

--- src/app/(routes)/dashboard/layout.jsx
+++ src/app/(routes)/dashboard/layout.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import SideBar from './_components/SideBar'
+import DashboardHeader from './_components/DashboardHeader'
 
 function DashboardLayout({ children }) {
     return (
         <div>
-            <div className='fixed md:w-64 hidden md:block shadow-sm'>
+            <div className='fixed md:w-64 hidden md:block  shadow-sm'>
                 <SideBar />
             </div>
-            <div className='md:ml-64 bg-blue-500'>
+            <div className='md:ml-64 '>
+                <DashboardHeader />
                 {children}
             </div>
 
